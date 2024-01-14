960eb8f1f643ca17d192cce25d1fc5c0256206ff
Gaurav Dubey <72222658+gauravdubey157@users.noreply.github.com>
2024-01-14 14:50:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ require('dotenv').config();
 
 const aboutContent = "Web platform to enhance community-driven learning, enabling users to share and explore tips, and book recommendations on a variety of technical topics, particularly DSA and Web Development.";
 
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
 
 
 const app = express();
@@ -72,6 +75,8 @@ app.get("/posts/:postId", async function(req, res) {
   }
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}`);
 });
+
